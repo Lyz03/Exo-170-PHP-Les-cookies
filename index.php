8339da79c65013385bacb69fd34e3e2f13fb29ee
Lyz03 <lizoe@netcourrier.com>
2022-01-25 15:31:39 +0100

    exo 170

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
  * 2. Affichez la valeur de ce cookie avec un simple echo.
  */
 // TODO Votre code ici.
-
+// setcookie("username", "lizoé");
+// echo "username -> " . $_COOKIE['username'];
 
 
 /**
@@ -14,3 +15,6 @@
  * 5. Créez une page bio.php et tentez d'afficher le cookie username sur cette page.
  */
 // TODO Votre code ici.
+setcookie('username', 'lizoé', time() + (3600 * 48) + (3600 * 2), '/', 'localhost');
+
+echo '<a href="bio.php">bio</a>';
